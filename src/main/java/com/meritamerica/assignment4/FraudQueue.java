9af9ea7ac7b1908e0c687c0e7bede8a0ed99d60b
MichelleArcelia <michellea.neptune@gmail.com>
2020-10-24 18:21:08 -0500

    Update FraudQueue.java

--- src/main/java/com/meritamerica/assignment4/FraudQueue.java
+++ src/main/java/com/meritamerica/assignment4/FraudQueue.java
@@ -1,19 +1,36 @@
 package com.meritamerica.assignment4;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FraudQueue {
+	
+	static List<Transaction> fraudList = new ArrayList<Transaction> ();
 
+	
 	public FraudQueue() {
-
+		
 	}
 
+// -------------- ADD TRANSACTION -------------- 
+	
 	public void addTransaction(Transaction transaction) {
-
+		
+		fraudList.add(transaction);
 	}
 
-	public Transaction getTransaction() {
+// -------------- TRANSACTION LIST -------------- 	
+	
+	public static List<Transaction> getTransactions() {
 		
-		
-		return null;
+		return FraudQueue.fraudList;
+	}
 
+// -------------- TRANSACTION SINGLE -------------- 		
+
+	public static Transaction getTransaction() {
+		
+		return FraudQueue.fraudList.get(0);
 	}
 }
