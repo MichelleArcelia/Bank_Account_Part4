3e6896e692e3efc6bd9149d5732f4b248e1ff582
MichelleArcelia <michellea.neptune@gmail.com>
2020-10-24 18:30:45 -0500

    CHANGES MADE - DepositTransaction

--- src/main/java/com/meritamerica/assignment4/DepositTransaction.java
+++ src/main/java/com/meritamerica/assignment4/DepositTransaction.java
@@ -3,9 +3,13 @@ package com.meritamerica.assignment4;
 public class DepositTransaction extends Transaction {
 
 	public DepositTransaction(BankAccount targetAccount, double amount) {
-		// TODO Auto-generated constructor stub
+		super(targetAccount, amount);
+		
 	}
 
+	// DOES THIS A NEED A DATE - java.util.Date date???
+	
+	
 	@Override
 	public void process()
 			throws NegativeAmountException, ExceedsAvailableBalanceException, ExceedsFraudSuspicionLimitException {
