ade20da3153b75c47f4beecc8e0d3c9e046b61ef
MichelleArcelia <michellea.neptune@gmail.com>
2020-10-25 01:29:44 -0500

    FINAL

--- src/main/java/com/meritamerica/assignment4/WithdrawTransaction.java
+++ src/main/java/com/meritamerica/assignment4/WithdrawTransaction.java
@@ -4,18 +4,17 @@
  * PART 4
  */
 
-
-
-
 package com.meritamerica.assignment4;
 
 import java.util.Date;
 
 public class WithdrawTransaction extends Transaction{
 
-	public WithdrawTransaction(BankAccount targetAccount, double amount) {
+	private double amount;
+
+	 public WithdrawTransaction(BankAccount targetAccount, double amount) {
 		
-		this.targetAccountNum = targetAccountNum;
+		this.targetAccount = targetAccount;
 		this.amount = amount;
 		
 	}
@@ -31,23 +30,21 @@ public class WithdrawTransaction extends Transaction{
 		}
 	
 		
-		else if (targetAccount.getBalance() > 250000) {
+		else if (targetAccount.getBalance() < amount) {
 			
-			throw new ExceedsAvailableBalanceException("Your deposit has exceeded the acceptable limit");
+			throw new ExceedsAvailableBalanceException("WARNING! Your deposit has exceeded the acceptable limit");
 		}
 		
 		
 		else if (amount > 1000) {
 			
-			throw new ExceedsFraudSuspicionLimitException("We are sorry this transaction can not completed");
+			throw new ExceedsFraudSuspicionLimitException("WARNING! We are sorry this transaction can not completed");
 			
 		}
 		
 		else {
 			
-			targetAccountNum.deposit(amount);
+			targetAccount.withdraw(amount);
 		}
-		
 	}
-
 }
